a1922768ef26387ba419c21e7a409bd972c8dd58
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2018-11-06 11:43:13 +0100

    Only show certain users friends

--- src/main/java/ch/hsr/domain/peer/Peer.java
+++ src/main/java/ch/hsr/domain/peer/Peer.java
@@ -2,20 +2,13 @@ package ch.hsr.domain.peer;
 
 import ch.hsr.domain.common.PeerId;
 import ch.hsr.domain.common.Username;
-import ch.hsr.domain.friend.Friend;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 @Data
-@EqualsAndHashCode (callSuper = true)
-@ToString (callSuper = true)
-public class Peer extends Friend {
+public class Peer {
 
+    private final PeerId peerId;
+    private final Username username;
     private final IpAddress ipAddress;
 
-    public Peer(PeerId peerId, Username username, IpAddress ipAddress) {
-        super(peerId, username);
-        this.ipAddress = ipAddress;
-    }
 }
