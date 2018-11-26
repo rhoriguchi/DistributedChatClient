f12b781f6e65e9bc84e0d20e4d1d56d1ba25417b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2018-11-26 23:25:57 +0100

    Reformat code

--- app/src/main/java/ch/hsr/dsa/view/ErrorBoxController.java
+++ app/src/main/java/ch/hsr/dsa/view/ErrorBoxController.java
@@ -1,17 +1,13 @@
 package ch.hsr.dsa.view;
 
-import ch.hsr.dsa.mapping.peer.PeerMapper;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 
 @Controller
-//TODO use for exceptions
 public class ErrorBoxController implements Thread.UncaughtExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ErrorBoxController.class);
@@ -23,14 +19,13 @@ public class ErrorBoxController implements Thread.UncaughtExceptionHandler {
     public ErrorBoxController() {
     }
 
-    private void showException(Exception e) {
-        errorMessage.setText(e.getMessage());
-    }
-
     @Override
     public void uncaughtException(Thread t, Throwable e) {
         LOGGER.error(e.getMessage(), e);
-
         showException((Exception) e);
+    }
+
+    private void showException(Exception e) {
+        errorMessage.setText(e.getMessage());
     }
 }
