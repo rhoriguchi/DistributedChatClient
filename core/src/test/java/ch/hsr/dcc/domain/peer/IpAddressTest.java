f90295b569d3f11e47eb435f94550f252128adb5
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2019-01-16 11:48:15 +0100

    Use Junit5

--- core/src/test/java/ch/hsr/dcc/domain/peer/IpAddressTest.java
+++ core/src/test/java/ch/hsr/dcc/domain/peer/IpAddressTest.java
@@ -1,6 +1,7 @@
 package ch.hsr.dcc.domain.peer;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
